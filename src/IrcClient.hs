42100afd30ae81a21cd27f54e7269b2fede9fc12
Andreas Ekeroot <andeke@gmail.com>
2016-08-22 02:16:31 +0200

    First working code.
    It's a quite rough prototype, but it does connect and say things.

---
+++ src/IrcClient.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE OverloadedStrings #-}
+module IrcClient where -- (connect, IRC(..), ircParser, privmsg, privmsgParser, runIRC, write) where
+
+import Control.Applicative ((<*), (*>))
+import Control.Monad (forever)
+import qualified Data.Attoparsec.ByteString as AP
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as B8
+import Data.Char (ord)
+import Data.Function (on)
+import Data.Maybe (maybe)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeLatin1)
+import Data.Word (Word8)
+import Network (PortID(..), connectTo)
+import System.IO (BufferMode(..), Handle, hClose, hFlush, hGetLine, hSetBuffering, stdout)
+import Text.Printf (hPrintf, printf)
+
+type Server   = T.Text
+type Port     = Int
+type Chan     = T.Text
+type Nick     = T.Text
+type LongName = T.Text
+
+type Reason = T.Text
+type Target = T.Text -- User or channel
+type Message = T.Text
+
+data IRC = Join Nick Chan
+         | Quit Nick Reason
+         | Part Nick Chan
+         | Ping Server
+         | Privmsg Target Message
+  deriving (Show)
+
+-- TODO: Find out how to send a /who and receive the results.
+
+toWord8 :: Char -> Word8
+toWord8 = toEnum . ord
+
+neq :: Char -> (Word8 -> Bool)
+neq = (/=) . toWord8
+
+-- | Take until \r\n has been reached. Throws newlines away.
+takeUntilEOL :: AP.Parser T.Text
+takeUntilEOL = decodeLatin1 <$> AP.takeWhile (neq '\r')
+
+-- crnlParser :: AP.Parser BS.ByteString
+-- crnlParser = AP.string "\r\n"
+
+nickParser :: AP.Parser Nick
+nickParser = AP.string ":" *> (decodeLatin1 <$> AP.takeWhile1 (neq '!')) <* AP.take 1
+
+hostParser :: AP.Parser T.Text
+hostParser = decodeLatin1 <$> AP.takeWhile1 (neq ' ') <* AP.take 1
+
+-- | Returns the name of the server that sent the ping.
+pingParser :: AP.Parser IRC
+pingParser = Ping <$> (AP.string "PING :" *> takeUntilEOL)
+
+-- TODO Refactor the joinParser, quitParser and partParser to be one baseparser with "children", this will save us quite some backtracking.
+
+chanParser :: AP.Parser Chan
+chanParser = do
+  AP.string "#"
+  s <- takeUntilEOL
+  return $ T.append "#" s
+
+joinParser :: AP.Parser IRC
+joinParser = do
+  nick <- nickParser
+  hostParser
+  AP.string "JOIN :"
+  chan <- chanParser
+  return $ Join nick chan
+
+partParser :: AP.Parser IRC
+partParser = do
+  nick <- nickParser
+  hostParser
+  AP.string "PART "
+  chan <- chanParser
+  return $ Part nick chan
+
+quitParser :: AP.Parser IRC
+quitParser = do
+  nick <- nickParser
+  hostParser
+  AP.string "QUIT :"
+  reason <- takeUntilEOL
+  return $ Quit nick reason
+
+targetParser :: AP.Parser Target
+targetParser = decodeLatin1 <$> AP.takeWhile1 (neq ' ') <* AP.take 1
+
+privmsgParser :: AP.Parser IRC
+privmsgParser = do
+  nickParser
+  hostParser
+  AP.string "PRIVMSG "
+  target  <- targetParser
+  AP.string ":"
+  message <- takeUntilEOL
+  return $ Privmsg target message
+
+ircParser :: AP.Parser IRC
+ircParser = AP.choice [pingParser, privmsgParser, joinParser, quitParser, partParser]
+
+-- | Connect to the server, set nick and all other needed parameters.
+-- Return the file handle.
+connect :: Server -> Port -> Chan -> Nick -> LongName -> IO Handle
+connect server port chan nick longname = do
+  printf "Connecting to %s ... " $ server
+  hFlush stdout
+  h <- connectTo (T.unpack server) (PortNumber $ fromIntegral $ port)
+  hSetBuffering h NoBuffering
+  putStrLn "Done."
+  setNick h nick longname
+  joinChannel h chan
+  return h
+
+-- | Set nickname
+setNick :: Handle -> Nick -> LongName -> IO ()
+setNick h nick longname = do
+  write h "NICK" nick
+  write h "USER" $ nick `T.append` " 0 * :" `T.append` longname
+  putStrLn $ "Changing nick to: " ++ (T.unpack nick)
+  putStrLn $ "Setting long name to: " ++ (T.unpack longname)
+
+-- | Join a channel
+-- TODO: We can join many channels but not have a clue of which channel we are in. This must be fixed!
+joinChannel :: Handle -> Chan -> IO ()
+joinChannel h chan = do
+  write h "JOIN" chan
+  putStrLn $ "Joining channel " ++ (T.unpack chan)
+
+pong :: Handle -> Server -> IO ()
+pong h server = write h "PONG: " server
+
+-- | Read one line from handle and parse it.
+processLine :: Handle -> IO IRC
+processLine h = do
+  input <- BS.hGetLine h
+  B8.putStrLn input
+  case AP.maybeResult $ AP.parse ircParser input of
+    Just    r -> return r
+    Nothing   -> putStrLn "ERROR: Couldn't parse line. Trying again with next line." >> processLine h
+
+-- | A run function for IRC
+-- Runs forever.
+runIRC :: Handle -> (IRC -> IO ()) -> IO ()
+runIRC h f = forever $ do
+  irc <- processLine h
+  case irc of
+    Ping server -> pong h server
+    x           -> f x
+
+-- | Send a message to a channel or user.
+privmsg :: Handle -> Chan -> Message -> IO ()
+privmsg h c s = do
+  write h "PRIVMSG" (c `T.append` " :" `T.append` s)
+
+-- | Write a command and a message to a handle.
+write :: Handle -> T.Text -> Message -> IO ()
+write h cmd msg = do
+  hPrintf h "%s %s\r\n" cmd msg
+  printf    "> %s %s\n" cmd msg
+
---
+++ src/Main.hs
@@ -0,0 +1,87 @@
+-- | The main program! \o/
+-- The IRC part here is heavily inspired by:
+-- https://wiki.haskell.org/Roll_your_own_IRC_bot
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Applicative ((<$>))
+import Control.Exception (bracket, bracket_)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.State.Lazy (get, gets, evalStateT, put, runState, StateT)
+import Data.List (init, isInfixOf, isPrefixOf)
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import Data.Text (Text)
+import Options.Applicative (Parser, auto, execParser, fullDesc, header, help, helper, info, long, metavar, option, progDesc, short, strOption, value)
+import System.Environment (getArgs)
+import System.Exit (ExitCode(..), exitWith)
+import System.IO (BufferMode(..), Handle, hClose, hFlush, hGetLine, hSetBuffering, stdout)
+import System.Random (StdGen, getStdGen, randomR)
+import Text.Printf (hPrintf, printf)
+
+import IrcClient
+
+data Options = Options {
+  optServer   :: T.Text,
+  optPort     :: Int,
+  optChan     :: T.Text,
+  optNick     :: T.Text,
+  optLongName :: T.Text
+}
+  deriving (Show)
+
+optParser :: Parser Options
+optParser = Options
+  <$> option auto
+    ( long "server"
+    <> short 's'
+    <> help "The domain or IP address of the server"
+    <> metavar "SERVER"
+    <> value "irc.dtek.se"
+    )
+  <*> option auto
+    ( long "port"
+    <> short 'p'
+    <> help "The port to connect to"
+    <> metavar "PORT"
+    <> value 6667
+    )
+  <*> option auto
+    ( long "channel"
+    <> short 'c'
+    <> help "The channel to join"
+    <> metavar "CHANNEL"
+    <> value "#dtek"
+    )
+  <*> option auto
+    ( long "nick"
+    <> short 'n'
+    <> help "The nick of the bot"
+    <> metavar "n"
+    <> value "HejBot"
+    )
+  <*> option auto
+    ( long "longname"
+    <> short 'l'
+    <> help "The long name of the bot"
+    <> metavar "LONGNAME"
+    <> value "Hej, the bot from internetz"
+    )
+
+main :: IO ()
+main = do
+  cfg <- execParser $ info (helper <*> optParser)
+                         ( fullDesc
+                         <> progDesc "The welcoming bot for #dtek"
+                         <> header "Welcome!"
+                         )
+
+  bracket (connect (optServer cfg) (optPort cfg) (optChan cfg) (optNick cfg) (optLongName cfg))
+          hClose
+          (\h -> runIRC h (eval h))
+
+eval :: Handle -> IRC -> IO ()
+eval h irc = case irc of
+               Join nick chan -> privmsg h chan $ nick `T.append` ": Hej och välkommen till #dtek, här hänger snälla datateknologer, stanna här en stund vetja och insup atmosfären!"
+               _              -> return ()
+
