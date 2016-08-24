dadbe12c9c928e5db2f104ff3347d399fde33a77
Andreas Ekeroot <andeke@gmail.com>
2016-08-24 18:52:49 +0200

    Made the whoparser and nickparsers more awesome!

--- src/IrcClient.hs
+++ src/IrcClient.hs
@@ -8,7 +8,8 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
-import Data.Char (isAlpha, isAlphaNum, isSpace)
+import Data.Char (isAlpha, isAlphaNum, isAsciiUpper, isDigit, isSpace)
+import Data.Ix (inRange)
 import Network (PortID(..), connectTo)
 import System.IO (BufferMode(..), Handle, hClose, hFlush, hGetLine, hSetBuffering, stdout)
 import Text.Printf (hPrintf, printf)
@@ -41,8 +42,19 @@ data IRC = Join Nick Chan
 takeUntilEOL :: AP.Parser T.Text
 takeUntilEOL = AP.takeWhile ((/= '\r'))
 
+-- All the special chars according to special in RFC 2812.
+-- https://tools.ietf.org/html/rfc2812#page-8
+specialChars :: Char -> Bool
+specialChars c = let x = fromEnum c
+                  in if ((0x5B, 0x60) `inRange` x) || ((0x7B, 0x7D) `inRange` x)
+                        then True
+                        else AP.inClass ";[]\\`_^{|}" c
+
 nickParser :: AP.Parser Nick
-nickParser = AP.takeWhile1 (/= '!')
+nickParser = do
+  frst <- AP.satisfy (\c -> isAlpha c || specialChars c)
+  rest <- AP.takeWhile (\c -> isAlphaNum c || specialChars c || (c == '-'))
+  return $ frst `T.cons` rest
 
 shortnameParser :: AP.Parser T.Text
 shortnameParser = foldr1 T.append <$> (AP.many1 $ AP.choice [AP.takeWhile1 isAlphaNum, "-"])
@@ -56,11 +68,17 @@ pingParser = Ping <$> (AP.string "PING :" *> takeUntilEOL)
 
 -- TODO Refactor the joinParser, quitParser and partParser to be one baseparser with "children", this will save us quite some backtracking.
 
+channelIdParser :: AP.Parser T.Text
+channelIdParser = do
+  exclamation <- AP.char '!'
+  rest        <- AP.count 5 (AP.satisfy (\c -> isAsciiUpper c || isDigit c))
+  return $ T.pack (exclamation : rest)
+
 chanParser :: AP.Parser Chan
 chanParser = do
-  "#"
-  s <- takeUntilEOL
-  return $ T.append "#" s
+  prefix  <- AP.choice ["#", "+", channelIdParser, "&"]
+  channel <- AP.takeWhile1 (AP.notInClass " ,:\r\n\NUL\BEL")
+  return $ T.append prefix s
 
 joinParser :: AP.Parser IRC
 joinParser = do
@@ -110,7 +128,7 @@ topicParser = hostParser *> " 332 " *> nickParser *> " " *> chanParser *> " :" *
 
 -- TODO: Add support for 366 and multiline name lists.
 whoParser :: AP.Parser IRC
-whoParser = hostParser *> " 353 " *> nickParser *> AP.choice [" = ", " * ", " @ "] *> chanParser *> " :" *> (Who <$> (AP.sepBy1 (AP.takeWhile1 (not . isSpace)) " "))
+whoParser = hostParser *> " 353 " *> nickParser *> AP.choice [" = ", " * ", " @ "] *> chanParser *> " :" *> (Who <$> AP.sepBy1 (AP.skipWhile (== '@') *> nickParser) " ")
 
 noticeParser :: AP.Parser IRC
 noticeParser = prefixParser *> " NOTICE " *> (Notice <$> takeUntilEOL)
